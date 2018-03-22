ea50c48b04e57bbe930c66cd6ee295af99bb2018
Radu Madiudin <rmadiudin@endava.com>
2018-03-22 11:45:26 +0200

    also works with orders, needs some refactoring

--- lab_1/src/main/java/mainClass.java
+++ lab_1/src/main/java/mainClass.java
@@ -1,4 +1,6 @@
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class mainClass {
     public static void main(String[] args) throws Exception{
@@ -6,10 +8,13 @@ public class mainClass {
         /*Creates a list HashMap de categoriii
         * when requested places each category in an order*/
 
-        URL url = new URL("https://evil-legacy-service.herokuapp.com" +
-                "/api/v101/categories/");
+        URL url = new URL("https://evil-legacy-service.herokuapp.com/" +
+                "api/v101/orders/");
 
-        URLConnectionReader.processInputURL(url);
+        URLConnectionReader link = new URLConnectionReader(url, "2003-05-11");
+        link.processInputURL();
+
+        //System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
 
     }
 }
